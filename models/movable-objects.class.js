dc7ed69add92001e28d3f2aff84c0864ea95c7ab
akin <akin@femka.de>
2025-11-23 12:54:37 +0100

    Fix: Applied bug fixes - Syntax errors resolved,Keyboard implemented, MovableObject extends DrawableObject

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -1,21 +1,14 @@
 class MovableObject extends DrawableObject {
-
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
     energy = 100;
     lastHit = 0;
-    // offset = {
-    //     top: 0,
-    //     left : 0,
-    //     right : 0,
-    //     bottom : 0
-    // }
 
     applyGravity() {
         setInterval(() => {
-            if(this.isAboveGround() || this.speedY > 0) {
+            if (this.isAboveGround() || this.speedY > 0) {
                 this.y -= this.speedY;
                 this.speedY -= this.acceleration;
             }
@@ -23,14 +16,13 @@ class MovableObject extends DrawableObject {
     }
 
     isAboveGround() {
-        return this.y < 150
+        return this.y < 180;
     }
-
 
     isColliding(mo) {
         return this.x + this.width > mo.x &&
             this.y + this.height > mo.y &&
-            this.x < mo.x &&
+            this.x < mo.x + mo.width &&
             this.y < mo.y + mo.height;
     }
 
@@ -38,11 +30,10 @@ class MovableObject extends DrawableObject {
         this.energy -= 5;
         if (this.energy < 0) {
             this.energy = 0;
-        }else {
+        } else {
             this.lastHit = new Date().getTime();
         }
     }
-
 
     isHurt() {
         let timepassed = new Date().getTime() - this.lastHit;
@@ -54,10 +45,7 @@ class MovableObject extends DrawableObject {
         return this.energy == 0;
     }
 
-
-
     playAnimation(images) {
-        //Walk Animation
         let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
@@ -71,11 +59,4 @@ class MovableObject extends DrawableObject {
     moveLeft() {
         this.x -= this.speed;
     }
-
-    // jump() {
-    //     this.speedY = 30;
-    // }
-
-
-
 }
--- models/world.class.js
+++ models/world.class.js
@@ -4,51 +4,30 @@ class World {
     canvas;
     ctx;
     keyboard;
-    camera_x = 0;
-    statusBar = new StatusBar();
 
     constructor(canvas, keyboard) {
-        this.ctx = canvas.getContext('2d');
+        this.ctx = canvas.getContext("2d");
         this.canvas = canvas;
         this.keyboard = keyboard;
-        this.setWorld();
         this.draw();
-        this.checkCollisions();
     }
-
-    setWorld() {
-        this.character.world = this;
-    }
-
-    checkCollisions() {
-        this.collisionCheckInterval = setInterval(() => {
-            this.level.enemies.forEach( (enemy) => {
-                if(this.character.isColliding(enemy) ) {
-                    this.character.hit();
-                    this.statusBar.setPercentage(this.character.energy);
-                }
-            });
-        }, 200);
-    }
-
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.translate(this.camera_x, 0);
-        this.addObjectsToMap(this.level.backgroundObjects);
-
-        this.ctx.translate(-this.camera_x, 0);
-        this.addToMap(this.statusBar);
-        this.ctx.translate(this.camera_x, 0);
-
-
         this.addToMap(this.character);
-        this.addObjectsToMap(this.level.clouds);
-        this.addObjectsToMap(this.level.enemies);
 
-        this.ctx.translate(-this.camera_x, 0);
+        this.level.clouds.forEach(cloud => {
+            this.addToMap(cloud);
+        });
 
+        this.level.backgroundObjects.forEach(bg => {
+            this.addToMap(bg);
+        });
+
+        this.level.enemies.forEach(enemy => {
+            this.addToMap(enemy);
+        });
 
         let self = this;
         requestAnimationFrame(function() {
@@ -56,35 +35,7 @@ class World {
         });
     }
 
-    addObjectsToMap(objects) {
-        objects.forEach(o => {
-            this.addToMap(o);
-        });
-    }
-
     addToMap(mo) {
-
-        if(mo.otherDirection) {
-            this.flipImage(mo);
-        }
-        mo.draw(this.ctx);
-        mo.drawFrame(this.ctx);
-
-        if(mo.otherDirection) {
-            this.flipImageBack(mo);
-        }
-    }
-
-    flipImage(mo) {
-        this.ctx.save();
-        this.ctx.translate(mo.width,0);
-        this.ctx.scale(-1, 1);
-        mo.x = mo.x * -1;
-
-    }
-
-    flipImageBack(mo) {
-        mo.x = mo.x * -1;
-        this.ctx.restore();
+        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
     }
 }
