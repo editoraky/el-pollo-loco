6ca29e6991fb9f9fc97e87fab42aaeca222c5ad9
akin <akin@femka.de>
2025-11-23 18:58:14 +0100

    changed the chicken.class.js smoothly

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,16 +1,8 @@
 class Chicken extends MovableObject {
-
-    width = 50;
-    height = 80;
     y = 360;
-    x = 490;
+    height = 60;
+    width = 60;
 
-    offset = {
-        top: 120,
-        bottom :30,
-        left : 40,
-        right : 40
-    }
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
@@ -20,19 +12,24 @@ class Chicken extends MovableObject {
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
-
         this.x = 200 + Math.random() * 500;
-        this.speed = 0.15 + Math.random() * 0.5;
+        this.speed = 0.15 + Math.random() * 0.25;
         this.animate();
     }
 
-
     animate() {
-        setInterval( () => {
-            this.moveLeft();
-        },1000 / 60);
+        this.moveLeft();
+        this.playWalkingAnimation();
+    }
 
-        setInterval (() => {
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
+    }
+
+    playWalkingAnimation() {
+        setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
         }, 200);
     }
