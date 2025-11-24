edf9a73715c7292140c0fa456dfcb2a5d3b1e05a
akin <akin@femka.de>
2025-11-24 05:52:17 +0100

    feat: setup character properties and enable gravity - set specific character dimensions and start position - set character movement speed - activate gravity logic in character constructor

--- models/character.class.js
+++ models/character.class.js
@@ -4,34 +4,66 @@ class Character extends MovableObject {
     speed = 10;
 
     IMAGES_IDLE = [
-        'img/2_character_pepe/1_idle/idle/I-1.png',
-        'img/2_character_pepe/1_idle/idle/I-2.png',
-        'img/2_character_pepe/1_idle/idle/I-3.png',
-        'img/2_character_pepe/1_idle/idle/I-4.png',
-        'img/2_character_pepe/1_idle/idle/I-5.png',
-        'img/2_character_pepe/1_idle/idle/I-6.png',
-        'img/2_character_pepe/1_idle/idle/I-7.png',
-        'img/2_character_pepe/1_idle/idle/I-8.png',
-        'img/2_character_pepe/1_idle/idle/I-9.png',
-        'img/2_character_pepe/1_idle/idle/I-10.png'
+        "img/2_character_pepe/1_idle/idle/I-1.png",
+        "img/2_character_pepe/1_idle/idle/I-2.png",
+        "img/2_character_pepe/1_idle/idle/I-3.png",
+        "img/2_character_pepe/1_idle/idle/I-4.png",
+        "img/2_character_pepe/1_idle/idle/I-5.png",
+        "img/2_character_pepe/1_idle/idle/I-6.png",
+        "img/2_character_pepe/1_idle/idle/I-7.png",
+        "img/2_character_pepe/1_idle/idle/I-8.png",
+        "img/2_character_pepe/1_idle/idle/I-9.png",
+        "img/2_character_pepe/1_idle/idle/I-10.png"
     ];
 
     IMAGES_WALKING = [
-        'img/2_character_pepe/2_walk/W-21.png',
-        'img/2_character_pepe/2_walk/W-22.png',
-        'img/2_character_pepe/2_walk/W-23.png',
-        'img/2_character_pepe/2_walk/W-24.png',
-        'img/2_character_pepe/2_walk/W-25.png',
-        'img/2_character_pepe/2_walk/W-26.png'
+        "img/2_character_pepe/2_walk/W-21.png",
+        "img/2_character_pepe/2_walk/W-22.png",
+        "img/2_character_pepe/2_walk/W-23.png",
+        "img/2_character_pepe/2_walk/W-24.png",
+        "img/2_character_pepe/2_walk/W-25.png",
+        "img/2_character_pepe/2_walk/W-26.png"
+    ];
+
+    IMAGES_JUMPING = [
+        "img/2_character_pepe/3_jump/J-31.png",
+        "img/2_character_pepe/3_jump/J-32.png",
+        "img/2_character_pepe/3_jump/J-33.png",
+        "img/2_character_pepe/3_jump/J-34.png",
+        "img/2_character_pepe/3_jump/J-35.png",
+        "img/2_character_pepe/3_jump/J-36.png",
+        "img/2_character_pepe/3_jump/J-37.png",
+        "img/2_character_pepe/3_jump/J-38.png",
+        "img/2_character_pepe/3_jump/J-39.png"
+    ];
+
+    IMAGES_HURT = [
+        "img/2_character_pepe/4_hurt/H-41.png",
+        "img/2_character_pepe/4_hurt/H-42.png",
+        "img/2_character_pepe/4_hurt/H-43.png"
+    ];
+
+    IMAGES_DEAD = [
+        "img/2_character_pepe/5_dead/D-51.png",
+        "img/2_character_pepe/5_dead/D-52.png",
+        "img/2_character_pepe/5_dead/D-53.png",
+        "img/2_character_pepe/5_dead/D-54.png",
+        "img/2_character_pepe/5_dead/D-55.png",
+        "img/2_character_pepe/5_dead/D-56.png",
+        "img/2_character_pepe/5_dead/D-57.png"
     ];
 
     world;
     keyboard;
 
     constructor() {
-        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+        super().loadImage("img/2_character_pepe/1_idle/idle/I-1.png");
         this.loadImages(this.IMAGES_IDLE);
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
+        this.loadImages(this.IMAGES_HURT);
+        this.loadImages(this.IMAGES_DEAD);
+        this.applyGravity();
         this.animate();
     }
 
